8733fd7f85fec7a3ac634deed319e337ee5a43c0
Benji York <benji@benjiyork.com>
2008-10-17 21:55:37 +0000

    merge in addition of url metadata
    (svn merge -c 92350 svn+ssh://svn.zope.org/repos/main/manuel/tags/1.0.0a2)

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='manuel',
     version='0',
+    url = 'http://pypi.python.org/pypi/manuel',
     packages=find_packages('src'),
     package_dir={'':'src'},
     zip_safe=False,
