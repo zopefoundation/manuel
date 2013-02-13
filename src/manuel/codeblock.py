9e242108313abf3f7c5d900c6f2bc0d62b79d388
Benji York <benji@benjiyork.com>
2013-02-13 21:50:39 +0000

    add support for docutils-style code blocks

--- src/manuel/codeblock.py
+++ src/manuel/codeblock.py
@@ -3,14 +3,15 @@ import manuel
 import textwrap
 
 CODEBLOCK_START = re.compile(
-    r'(^\.\.\s*(invisible-)?code-block::?\s*python\b(?:\s*\:\w+\:)*)',
+    r'(^\.\.\s*(invisible-)?code(-block)?::?\s*python\b(?:\s*\:\w+\:.*\n)*)',
     re.MULTILINE)
 CODEBLOCK_END = re.compile(r'(\n\Z|\n(?=\S))')
 
 
 class CodeBlock(object):
-    def __init__(self, code):
+    def __init__(self, code, source):
         self.code = code
+        self.source = source
 
 
 def find_code_blocks(document):
@@ -20,7 +21,7 @@ def find_code_blocks(document):
         source_location = '%s:%d' % (document.location, region.lineno)
         code = compile(source, source_location, 'exec', 0, True)
         document.claim_region(region)
-        region.parsed = CodeBlock(code)
+        region.parsed = CodeBlock(code, source)
 
 
 def execute_code_block(region, document, globs):
